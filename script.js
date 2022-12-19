c75a281da5e6e722d16b54fc38b650ea359dc4d1
MantashaGera <113764450+MantashaGera@users.noreply.github.com>
2022-12-19 19:28:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const jobListings = [
+  {
+    title: "Software Engineer",
+    company: "Acme Corp",
+    location: "San Francisco, CA",
+    description: "We are seeking a skilled software engineer to join our team. The ideal candidate will have experience with Python and JavaScript.",
+  },
+  {
+    title: "Product Manager",
+    company: "Acme Corp",
+    location: "San Francisco, CA",
+    description: "We are seeking a talented product manager to join our team
